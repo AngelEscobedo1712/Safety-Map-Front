cef8d43f99ad89648ac6df382e62ba26e4aa1ced
Pieter Dietrich <pieter@dietrich-fam.de>
2023-06-26 15:28:56 -0600

    first draft for the connected frontend and backend  for the historical map

--- app/test_app.py
+++ app/test_app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import requests
+import pandas as pd
+import os
+
+API_HOST = os.getenv("API_HOST")
+
 
 # Safety Map Front
 st.markdown("""
@@ -23,49 +28,70 @@ st.title("Historical crime data")
 map = folium.Map(location=[19.4326, -99.1332], zoom_start=11, tiles='Stamen Toner')
 
 # Fetch neighborhoods from the backend
-response = requests.get("http://localhost:8000/neighborhoods")
+response = requests.get(API_HOST + "/neighborhoods")
 neighborhood = response.json()["neighborhoods"]
+
+
+
 
 # Add dropdowns
 dropdown_values = {
     'Neighborhood': neighborhood,
     'Year': ['ALL', 2019, 2020, 2021, 2022, 2023],
-    'Month': ['ALL'] + list(range(1, 13)),
+    'Month': ['ALL'] + ["Enero","Febrero" ,"Marzo" ,"Abril" ,"Mayo" ,"Junio" ,"Julio" ,"Agosto" ,"Septiembre" ,"Octubre" ,"Noviembre" ,"Diciembre"],
     'Category': ['ALL', 'fraud', 'threats', 'threats', 'burglary', 'homicide',
                   'sexual crime', 'property damage', 'domestic violence', 'danger of well-being',
                   'robbery with violence', 'robbery without violence']
 }
 
+
 selected_values = {}
 for dropdown_label, dropdown_options in dropdown_values.items():
     selected_values[dropdown_label] = st.selectbox(dropdown_label, dropdown_options)
 
-# Make API request to the backend to get crime data
-response = requests.get(
-    "http://localhost:8000/crime-data",
-    params={
+
+# Initialize the search_executed flag
+if 'search_executed' not in st.session_state:
+    st.session_state.search_executed = False
+
+markers_data = []
+
+if st.button('Search'):
+    # Make API request to the backend to get historical data
+    api_url = API_HOST + "/get_historical_data"
+
+    params = {
         'neighborhood': selected_values['Neighborhood'],
         'year': selected_values['Year'],
         'month': selected_values['Month'],
         'category': selected_values['Category']
     }
-)
-data = response.json()
 
-# Create a Folium map
-if data:
-    map_center = [data[0]['Latitude'], data[0]['Longitude']]
+    print('Searching crimes...')
+    response = requests.get(api_url, params=params)
+    data = response.json()["data"]
+
+    # Create a Pandas DataFrame from the data
+    dataframe = pd.DataFrame(data)
+
+    if data:
+        for row in data:
+            marker = folium.Marker([row['Latitude'], row['Longitude']])
+            markers_data.append(marker)
+            marker.add_to(map)
+        # Set the flag to indicate that a search has been executed
+        st.session_state.search_executed = True
+    else:
+        st.session_state.search_executed = False
+
+    st_folium(map, width=700)
+
 else:
-    map_center = [19.4326, -99.1332]  # Default center if no data is available
+    st.write('No search yet')
 
-map = folium.Map(location=map_center, zoom_start=11, tiles='Stamen Toner')
-
-# Add markers to the map
-if data:
-    for row in data:
-        folium.Marker([row['Latitude'], row['Longitude']]).add_to(map)
-else:
+# Display the message if no crime was committed and a search has been executed
+if not st.session_state.search_executed and st.session_state.search_executed is not False:
     st.markdown(""" ## NO CRIME WAS COMMITTED """)
 
-# Display the map
-st_folium(map, width=700)
+# Store the markers in the session state to persist them
+st.session_state.markers_data = markers_data
