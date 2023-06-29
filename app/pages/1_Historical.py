a57e572371a85fbe7967257bbd1c1522ff5a58e7
angelescobedo <angelescobedo1712@gmail.com>
2023-06-29 16:07:53 -0600

    new changes to front

--- app/pages/1_Historical.py
+++ app/pages/1_Historical.py
@@ -5,74 +5,97 @@ import requests
 import pandas as pd
 import os
 
+
+st.set_page_config(layout="wide")
+
+
+margins_css = """
+    <style>
+        .main > div {
+            padding-left: 2rem;
+            padding-right: 2rem;
+            padding-top: 2rem;
+        }
+    </style>
+"""
+
+st.markdown(margins_css, unsafe_allow_html=True)
+
 API_HOST = os.getenv("API_HOST")
 
 # Add map
-st.title("Historical crime data")
+st.title("Historical 📅 crime data 📜📍")
 map = folium.Map(location=[19.4326, -99.1332], zoom_start=11, tiles='Stamen Toner')
 
-# Fetch neighborhoods from the backend
-if 'neighborhoods' not in st.session_state:
-    response = requests.get(API_HOST + "/neighborhoods")
-    neighborhoods = response.json()["neighborhoods"]
-    st.session_state.neighborhoods = neighborhoods
-else:
-    neighborhoods = st.session_state.neighborhoods
 
-# Add checkboxes
-checkbox_values = {
-    'Neighborhood': neighborhoods,
-    'Year': ['ALL', 2019, 2020, 2021, 2022, 2023],
-    'Month': ['ALL'] + ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-    'Category': ['ALL', 'fraud', 'threats', 'threats', 'burglary', 'homicide',
-                  'sexual crime', 'property damage', 'domestic violence', 'danger of well-being',
-                  'robbery with violence', 'robbery without violence']
-}
+col1, col2 = st.columns([1,3])
 
-selected_values = {}
-for checkbox_label, checkbox_options in checkbox_values.items():
-    selected_values[checkbox_label] = st.multiselect(checkbox_label, checkbox_options)
+with col1:
+    # Fetch neighborhoods from the backend
+    if 'neighborhoods' not in st.session_state:
+        response = requests.get(API_HOST + "/neighborhoods")
+        neighborhoods = response.json()["neighborhoods"]
+        st.session_state.neighborhoods = neighborhoods
+    else:
+        neighborhoods = st.session_state.neighborhoods
 
-# Initialize the search_executed flag
-if 'search_executed' not in st.session_state:
-    st.session_state.search_executed = False
-    st.session_state.data = []
-
-markers_data = []
-
-if st.button('Search'):
-    # Make API request to the backend to get historical data
-    api_url = API_HOST + "/get_historical_data"
-
-    year = selected_values['Year'] if 'ALL' not in selected_values['Year'] else None
-    params = {
-        'neighborhoods': selected_values['Neighborhood'],
-        'years': year,
-        'months': selected_values['Month'],
-        'categories': selected_values['Category']
+    # Add checkboxes
+    checkbox_values = {
+        'Neighborhood': neighborhoods,
+        'Year': ['ALL', 2019, 2020, 2021, 2022, 2023],
+        'Month': ['ALL'] + ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+        'Category': ['ALL', 'fraud', 'threats', 'threats', 'burglary', 'homicide',
+                    'sexual crime', 'property damage', 'domestic violence', 'danger of well-being',
+                    'robbery with violence', 'robbery without violence']
     }
 
-    st.write('Searching crimes...')
-    response = requests.post(api_url, json=params)
-    #print(response.content)
-    # Create a Pandas DataFrame from the data
-    st.session_state.data = response.json()["data"]
-    st.session_state.search_executed = True
-else:
-    st.write('No search yet')
+    selected_values = {}
+    for checkbox_label, checkbox_options in checkbox_values.items():
+        selected_values[checkbox_label] = st.multiselect(checkbox_label, checkbox_options)
 
-if st.session_state.search_executed:
-    data = st.session_state.data
-    dataframe = pd.DataFrame(data)
-    if data:
-        for row in data:
-            marker = folium.Marker([row['Latitude'], row['Longitude']], tooltip=row['Category'])
-            markers_data.append(marker)
-            marker.add_to(map)
-        # Set the flag to indicate that a search has been executed
-        folium_static(map, width=700)
-        st.session_state.markers_data = markers_data
+    # Initialize the search_executed flag
+    if 'search_executed' not in st.session_state:
+        st.session_state.search_executed = False
+        st.session_state.data = []
 
+    markers_data = []
+
+    if st.button('Search 🔍'):
+        # Make API request to the backend to get historical data
+        api_url = API_HOST + "/get_historical_data"
+
+        year = selected_values['Year'] if 'ALL' not in selected_values['Year'] else None
+        params = {
+            'neighborhoods': selected_values['Neighborhood'],
+            'years': year,
+            'months': selected_values['Month'],
+            'categories': selected_values['Category']
+        }
+
+        st.write('Searching crimes...')
+        response = requests.post(api_url, json=params)
+        #print(response.content)
+        # Create a Pandas DataFrame from the data
+        st.session_state.data = response.json()["data"]
+        st.session_state.search_executed = True
     else:
-        # Display the message if no crime was committed and a search has been executed
-        st.markdown(""" ## NO CRIME WAS COMMITTED """)
+        st.write('No search yet')
+
+
+with col2:
+
+    if st.session_state.search_executed:
+        data = st.session_state.data
+        dataframe = pd.DataFrame(data)
+        if data:
+            for row in data:
+                marker = folium.Marker([row['Latitude'], row['Longitude']], tooltip=row['Category'])
+                markers_data.append(marker)
+                marker.add_to(map)
+            # Set the flag to indicate that a search has been executed
+            folium_static(map, width=700)
+            st.session_state.markers_data = markers_data
+
+        else:
+            # Display the message if no crime was committed and a search has been executed
+            st.markdown(""" ## NO CRIME WAS COMMITTED """)
--- app/pages/2_Forecasting_Crimes.py
+++ app/pages/2_Forecasting_Crimes.py
@@ -10,8 +10,20 @@ import folium
 #import ipywidgets as widgets
 from folium.plugins import HeatMap, HeatMapWithTime
 import json
-#from datetime import datetime, timedelta
 
+st.set_page_config(layout="wide")
+
+margins_css = """
+    <style>
+        .main > div {
+            padding-left: 2rem;
+            padding-right: 2rem;
+            padding-top: 2rem;
+        }
+    </style>
+"""
+
+st.markdown(margins_css, unsafe_allow_html=True)
 
 API_HOST = os.getenv("API_HOST")
 
@@ -19,84 +31,87 @@ api_url_download_polygons = API_HOST + "/download_polygons"
 response_download_polygons = requests.get(api_url_download_polygons).json()
 
 # Add map
-st.title("Forecasting Crimes from July 2023 to December 2023")
+st.title("Forecasting Crimes 🫣 🔫🥸")
+st.subheader("July 2023 - December 2023")
 
 
-# Add checkboxes
-checkbox_values = {
-    'Month': ["07", "08", "09", "10", "11", "12"],
-    'Category': ['fraud', 'threats', 'burglary', 'homicide',
-                  'sexual_crime', 'property_damage', 'domestic_violence', 'danger_of_well-being',
-                  'robbery_with_violence', 'robbery_without_violence','score']
-}
+col1, col2 = st.columns([1,3])
 
-selected_values = {}
-for checkbox_label, checkbox_options in checkbox_values.items():
-    selected_values[checkbox_label] = st.selectbox(checkbox_label, checkbox_options)
-
-# Initialize the search_executed flag
-if 'search_executed' not in st.session_state:
-    st.session_state.search_executed = False
-    st.session_state.data = []
-
-if st.button('Search'):
-    # Make API request to the backend to get Forecasting data
-    api_url = API_HOST + "/get_crimes"
-
-    year_month = f"2023-{selected_values['Month']}-01"
-    category = selected_values['Category']
-
-    year_month_search = year_month if 'ALL' not in year_month else None
-    params = {
-        'year_month': year_month_search,
-        'category': category
+with col1:
+    # Add checkboxes
+    checkbox_values = {
+        'Month': ["07", "08", "09", "10", "11", "12"],
+        'Category': ['fraud', 'threats', 'burglary', 'homicide',
+                    'sexual_crime', 'property_damage', 'domestic_violence', 'danger_of_well-being',
+                    'robbery_with_violence', 'robbery_without_violence','score']
     }
 
-    response = requests.get(api_url, params=params)
-    # Create a Pandas DataFrame from the data
-    st.session_state.data = response.json()["data"]
-    #st.write(response.json()["data"])
-    st.session_state.search_executed = True
+    selected_values = {}
+    for checkbox_label, checkbox_options in checkbox_values.items():
+        selected_values[checkbox_label] = st.selectbox(checkbox_label, checkbox_options)
 
+    # Initialize the search_executed flag
+    if 'search_executed' not in st.session_state:
+        st.session_state.search_executed = False
+        st.session_state.data = []
 
+    if st.button('Search 🔍'):
+        # Make API request to the backend to get Forecasting data
+        api_url = API_HOST + "/get_crimes"
 
+        year_month = f"2023-{selected_values['Month']}-01"
+        category = selected_values['Category']
 
-else:
-    st.write('No search yet')
+        year_month_search = year_month if 'ALL' not in year_month else None
+        params = {
+            'year_month': year_month_search,
+            'category': category
+        }
 
-if st.session_state.search_executed:
-    data = st.session_state.data
-
-    dataframe = pd.DataFrame(data)
-
-
-    category = selected_values['Category']
-
-    if data:
-
-
-    #test if everything worked and build a map
-        #poly_geo = 'local_geo.json'
-        #st.write(response_download_polygons.json())
-
-        map = folium.Map(location=[19.4326, -99.1332], zoom_start=11, tiles='Stamen Toner')
-
-        folium.Choropleth(
-            geo_data=response_download_polygons,
-            name="choropleth",
-            data=dataframe,
-            columns=["code", category],
-            key_on="feature.properties.geo_point_2d.lat",
-            fill_color="YlOrRd",
-            fill_opacity=0.7,
-            line_opacity=0.2,
-            legend_name=category,
-        ).add_to(map)
-
-        folium.LayerControl().add_to(map)
-
-        folium_static(map, width=700)
-
+        response = requests.get(api_url, params=params)
+        # Create a Pandas DataFrame from the data
+        st.session_state.data = response.json()["data"]
+        #st.write(response.json()["data"])
+        st.session_state.search_executed = True
     else:
-        # Display the message if no crime was committed and a search has been executed
-        st.markdown(""" ## NO CRIME WAS PREDICTED """)
+        st.write('')
+
+
+with col2:
+
+    if st.session_state.search_executed:
+        data = st.session_state.data
+
+        dataframe = pd.DataFrame(data)
+
+
+        category = selected_values['Category']
+
+        if data:
+
+
+        #test if everything worked and build a map
+            #poly_geo = 'local_geo.json'
+            #st.write(response_download_polygons.json())
+
+            map = folium.Map(location=[19.4326, -99.1332], zoom_start=11, tiles='Stamen Toner')
+
+            folium.Choropleth(
+                geo_data=response_download_polygons,
+                name="choropleth",
+                data=dataframe,
+                columns=["code", category],
+                key_on="feature.properties.geo_point_2d.lat",
+                fill_color="YlOrRd",
+                fill_opacity=0.7,
+                line_opacity=0.2,
+                legend_name=category,
+            ).add_to(map)
+
+            folium.LayerControl().add_to(map)
+
+            folium_static(map, height=500, width=750)
+
+        else:
+            # Display the message if no crime was committed and a search has been executed
+            st.markdown(""" ## NO CRIME WAS PREDICTED """)
