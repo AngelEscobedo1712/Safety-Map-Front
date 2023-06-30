05e4428e75078cf3ba5beb4cb123e737f887df49
Pieter Dietrich <pieter@dietrich-fam.de>
2023-06-30 12:56:38 -0600

    forecasting plots works now

--- app/pages/3_Plots.py
+++ app/pages/3_Plots.py
@@ -107,27 +107,80 @@ with tab1:
 
     with col2:
         if st.session_state.plot_search:
-                data = st.session_state.data
-                dataframe = pd.DataFrame(data)
-                dataframe['Month'] = dataframe['Month'].replace(month_mapping_swapped)
-                dataframe['datetime'] = pd.to_datetime(dataframe['Year'].astype(int).astype(str) + ' ' + dataframe['Month'], format='%Y %B')
-
-                # Create the Altair chart
-
-                selection = alt.selection_interval(bind='scales')
-                chart = alt.Chart(dataframe).mark_line().encode(
+            data = st.session_state.data
+            dataframe = pd.DataFrame(data)
+            dataframe['Month'] = dataframe['Month'].replace(month_mapping_swapped)
+            dataframe['datetime'] = pd.to_datetime(dataframe['Year'].astype(int).astype(str) + ' ' + dataframe['Month'], format='%Y %B')
+            # Create the Altair chart
+            selection = alt.selection_interval(bind='scales')
+            chart = alt.Chart(dataframe).mark_line().encode(
                 x='datetime:T',
                 y=alt.Y('TotalCrimes', scale=alt.Scale(domain=(1,10)), axis=alt.Axis(tickMinStep=1, labelOverlap=True)),
-                        color='Category'
-                ).add_params(
-                    selection
-                )
+                    color='Category'
+            ).add_params(
+                selection
+            )
 
-                # Display the chart using Streamlit
-                st.altair_chart(chart, use_container_width=True)
+            st.altair_chart(chart, use_container_width=True)
 
 
 with tab2:
-    #Title of the page
+    # Title of the page
     st.title("Forecasting Plots 📉")
     st.subheader("Here you can play with some plots for the Forecasting data")
+
+    # Initialize the plot_search flag
+    if 'predictions_search' not in st.session_state:
+        st.session_state.predictions_search = False
+        st.session_state.prediction_data = None  # Initialize prediction_data as None
+
+    month_mapping_two = {
+        "July": '07',
+        "August": '08',
+        "September": '09',
+        "October": '10',
+        "November": '11',
+        "December": '12'
+    }
+
+    month_mapping_two_swapped = {value: key for key, value in month_mapping_two.items()}
+
+    checkbox_values_predict = {
+        'Category': ['fraud', 'threats', 'burglary', 'homicide',
+                     'sexual_crime', 'property_damage', 'domestic_violence', 'danger_of_well_being',
+                     'robbery_with_violence', 'robbery_without_violence', 'score'],
+    }
+
+    selected_values_predict = {}
+    for checkbox_label_predict, checkbox_options_predict in checkbox_values_predict.items():
+        selected_values_predict[checkbox_label_predict] = st.multiselect(checkbox_label_predict,
+                                                                         checkbox_options_predict)
+
+        selected_category = selected_values_predict.get('Category')
+
+    if st.session_state.prediction_data is None:
+        response_predict = requests.get(API_HOST + "/get_plot_prediction_data")
+        prediction_data = response_predict.json()["data"]
+        st.session_state.prediction_data = pd.DataFrame(prediction_data)
+    else:
+        if selected_category == []:
+            st.warning("Please select at least one crime Category")
+            st.empty()
+
+    # Get the selected value from selected_values_predict
+    if selected_category:
+        selected_value = [f"total_{category}" for category in selected_category] + ["year_month"]
+
+        df = st.session_state.prediction_data[selected_value].melt(id_vars='year_month', value_name='Crimes')
+
+        # Create the Altair chart
+        selection_new = alt.selection_interval(bind='scales')
+        chart_predict = alt.Chart(df).mark_line().encode(
+            x='year_month',
+            y='Crimes', color='variable'
+        ).add_params(
+            selection_new
+        )
+
+
+        st.altair_chart(chart_predict, use_container_width=True)
